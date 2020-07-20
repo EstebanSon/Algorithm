c6963d4923ec1fe32149ec8f00b459735ac5c430
Esteban21 <isomorphism1126@gmail.com>
2020-07-20 17:37:45 +0900

    Add Searching Algorithm

---
+++ Algorithm/Algorithm/Searching.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Searching
+    {
+        public static int LinearSearch(int[] arr, int nLength, int value)
+        {
+            int rtValue = 0;
+
+            for(int i = 0; i < nLength; i++)
+            {
+                if (arr[i] == value)
+                    rtValue = i;
+            }
+
+            return rtValue;
+        }
+
+        public static int BinarySearch(int[] arr, int init, int end, int value)
+        {
+            int rtValue = -1;
+
+            if(end >= 1)
+            {
+                int mid = (end + 1) / 2;
+                if (arr[mid] == value)
+                    return rtValue= mid;
+
+                if (arr[mid] > value)
+                    return BinarySearch(arr, init, mid - 1, value);
+
+                return BinarySearch(arr, mid + 1, end, value);
+            }
+            return rtValue;
+        }
+    }
+}
