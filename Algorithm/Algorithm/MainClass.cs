121f73e673daeeb7f65e18e85dda443f693420e4
Esteban21 <isomorphism1126@gmail.com>
2020-07-20 09:12:21 +0900

    Create Default Algorithm

---
+++ Algorithm/Algorithm/MainClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            Random rand = new Random();
+
+            for(int i = 0; i < 10; i++)
+            {
+                arr[i] = rand.Next(10) + 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if(arr[i] == arr[j])
+                    {
+                        i--;
+                    }
+
+                    
+                }
+            }
+
+            //string value = Console.ReadLine();
+            Debug.WriteLine("Before");
+            for(int k = 0; k < 10; k++)
+            {
+                Debug.WriteLine(arr[k]);
+            }
+
+            // 선택정렬
+            //Sorting.SelectionSort(arr);
+
+            // 삽입정렬
+            //Sorting.InsertSort(arr);
+
+            // 버블정렬
+            Sorting.BubbleSort(arr);
+
+            Debug.WriteLine("After");
+            for (int k = 0; k < 10; k++)
+            {
+                Debug.WriteLine(arr[k]);
+            }
+        }
+    }
+}
---
+++ Algorithm/Algorithm/Sorting.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    class Sorting
+    {
+        public static int[] SelectionSort(int[] data)
+        {
+            for(int i = 0; i < data.Length; i++)
+            {
+                for(int j = i + 1; j < data.Length; j++)
+                {
+                    if(data[i] > data[j])
+                    {
+                        int temp = data[j];
+                        data[j] = data[i];
+                        data[i] = temp;
+                    }
+                }
+            }
+            return data;
+        }
+
+        public static int[] InsertSort(int[] data)
+        {
+            for(int i = 0; i < data.Length; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if(data[i] < data[j])
+                    {
+                        int temp = data[i];
+                        data[i] = data[j];
+                        data[j] = temp;
+                    }
+                }
+            }
+
+            return data;
+        }
+
+        public static int[] MergeSort(int[] data)
+        {
+            int half = data.Length / 2;
+            int top = data.Length - half;
+            int bot = data.Length - top;
+
+            for(int i = 0; i < data.Length; i += 2)
+            {
+                if(data[i+1] < data[i])
+                {
+                    int temp = data[i+1];
+                    data[i+1] = data[i];
+                    data[i] = temp;
+                }
+            }
+            
+            return data;
+        }
+
+        public static int[] BubbleSort(int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 1, k = 0; j < data.Length; j++, k++)
+                {
+                    if (data[j] < data[k])
+                    {
+                        int temp = data[j];
+                        data[j] = data[k];
+                        data[k] = temp;
+                    }
+                }
+            }
+
+            return data;
+        }
+    }
+}
